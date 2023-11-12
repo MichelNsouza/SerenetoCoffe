a459e28e4ce3a4038857c63c2a706388f699823b
Michel Souza <91084191+MichelNsouza@users.noreply.github.com>
2023-11-12 18:27:13 -0300

    Update conexao-bd.php

--- src/conexao-bd.php
+++ src/conexao-bd.php
@@ -1,3 +1,2 @@
 <?php
-
-$pdo = new PDO('mysql:host=containers-us-west-150.railway.app:7082;dbname=railway', 'root', '9mdILYHiDmVoLyexQF7x');
+$pdo = new PDO('pgsql:host=dpg-ckc82bect0pc73afvsug-a.oregon-postgres.render.com','testebd_user','PASSWORD');
